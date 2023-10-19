588db727a6baf05bd8e9eaa3c290ab83413f66e3
perforb <dev.perfumed.garden@gmail.com>
2023-10-19 21:30:36 +0900

    feat: add a func to call the weather API

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,11 +2,16 @@ import {useState} from "react";
 
 const Form = () => {
   const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch("http://api.weatherapi.com/v1/current.json?key=86e7e071a084414b836120742231910&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
   return (
     <form>
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 };
