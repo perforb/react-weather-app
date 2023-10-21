482bcc30ff3c6ae8c1eeaa96de4a0b55a097952e
perforb <dev.perfumed.garden@gmail.com>
2023-10-21 22:23:59 +0900

    feat: add .env

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   });
   const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch(`https://api.weatherapi.com/v1/current.json?key=86e7e071a084414b836120742231910&q=${city}&aqi=no`)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&aqi=no`)
       .then(res => res.json())
       .then(data => {
         setResults({
